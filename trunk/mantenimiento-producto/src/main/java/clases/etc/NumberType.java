a5f36a7e30400efdf3b54eea65e52816c1738ed2
cesar.dl88@gmail.com <cesar.dl88@gmail.com@50497599-b41c-d8ec-f490-137938bfd016>
2014-10-07 18:28:49 +0000

    Agregando enum de tipo de numero

---
+++ trunk/mantenimiento-producto/src/main/java/clases/etc/NumberType.java
@@ -0,0 +1,18 @@
+package clases.etc;
+
+/**
+ *
+ * @author cesardiaz
+ */
+public enum NumberType {
+
+    /**
+     * Formato de n&uacute;mero entero
+     */
+    ENTERO,
+    /**
+     * Formato de n&uacute;mero real
+     */
+    REAL
+
+}
