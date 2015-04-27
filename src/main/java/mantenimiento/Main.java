238fcf2b09856f5f79b151314f8c3a6b1279c75b
César Díaz <cesar.diaz@scytl.com>
2015-04-27 16:29:35 -0500

    Change logger dependency

--- src/main/java/mantenimiento/Main.java
+++ src/main/java/mantenimiento/Main.java
@@ -2,6 +2,9 @@ package mantenimiento;
 
 import java.io.IOException;
 import java.util.Properties;
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
 import org.apache.log4j.Logger;
 
 /**
@@ -17,13 +20,26 @@ public class Main {
      */
     public static void main(String[] args) {
         try {
+            UIManager.setLookAndFeel(UIManager
+                    .getSystemLookAndFeelClassName());
+        } catch (ClassNotFoundException e) {
+            log.error("Error with class", e);
+        } catch (InstantiationException e) {
+            log.error("Error while instantiation", e);
+        } catch (IllegalAccessException e) {
+            log.error("Error illegal access", e);
+        } catch (UnsupportedLookAndFeelException e) {
+            log.error("Error loading look and feel", e);
+        }
+
+        try {
             Properties prop = new Properties();
             prop.load(Main.class
                     .getResourceAsStream("/app.properties"));
 
             final String strRuta = System.getProperty("user.home") + prop.getProperty("file.data");
 
-            javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            SwingUtilities.invokeLater(new Runnable() {
 
                 @Override
                 public void run() {
